aa1628b93e5dda081b67e06b674f0761f912aa66
Ian Njari <iannjari@gmail.com>
2021-07-12 21:03:36 +0300

    Add inventory dict

--- Lesson/app1.py
+++ Lesson/app1.py
@@ -10,4 +10,15 @@ def home():
 def about():
     return {'Data':'Testing About'}
 
-
+inventory= {
+    1: {
+        "name":"milk",
+        "price":3
+        "brand": "KCC"
+        },
+    2: {
+        "name":"soda",
+        "price":8
+        "brand": "Cocacola"
+        }
+}
