1bc3eb33235e6f910bf3cc1df5232ddc91338325
Ian Njari <iannjari@gmail.com>
2021-07-13 12:43:46 +0300

    Post method

--- Lesson/app1.py
+++ Lesson/app1.py
@@ -1,7 +1,16 @@
+from typing import Optional
 from fastapi import FastAPI, Path
 from fastapi.param_functions import Query
-
+from typing import Optional
+from pydantic import BaseModel
 app= FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str]= None
+
 
 @app.get("/")
 def home():
@@ -25,9 +34,10 @@ inventory= {
 }
 
 @app.get("/get-item/{item_id}")
-def get_item(item_id:int = Path(None,description='The ID of the item you would like to view',gt=0,lt=2)):
-    return inventory[item_id]
-
+def get_item(item_id:int = Path(None,description='The ID of the item you would like to view',gt=0)):
+    if item_id in inventory:
+        return inventory[item_id]
+    return {'Data Not Found'}
 @app.get("/get-by-name")
 def get_item_by_name(name:str =Query(None,title='Name',description='Item name')):
     for item_id in inventory:
@@ -35,3 +45,9 @@ def get_item_by_name(name:str =Query(None,title='Name',description='Item name'))
             return inventory[item_id]
     return {'Data Not Found'}
 
+@app.post("/create-item/{item_id}")
+def create_item(item_id:int,item:Item):
+    if item_id in inventory:
+        return {'Error':'Item ID already exists'}
+    inventory[item_id]= {'name':item.name,'price':item.price,'brand':item.brand}
+    return inventory[item_id]
