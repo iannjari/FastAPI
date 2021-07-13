2df4f930fa1c4198cab3999c50c3e5ee04c9e8ff
Ian Njari <iannjari@gmail.com>
2021-07-13 13:10:32 +0300

    Put method

--- Lesson/app1.py
+++ Lesson/app1.py
@@ -11,6 +11,11 @@ class Item(BaseModel):
     price: float
     brand: Optional[str]= None
 
+class UpdateItem(BaseModel):
+    name: Optional[str]= None
+    price: Optional[float]= None
+    brand: Optional[str]= None
+
 
 @app.get("/")
 def home():
@@ -41,4 +46,19 @@ def create_item(item_id:int,item:Item):
     if item_id in inventory:
         return {'Error':'Item ID already exists'}
     inventory[item_id]= item
+    return inventory[item_id]
+
+@app.put("/update-item/{item_id}")
+def update_item(item_id:int,item:UpdateItem):
+    
+    if item_id not in inventory:
+        return {'Error':'Item ID does not exist'}
+    
+    if item.name!= None:
+        inventory[item_id].name= item.name
+    if item.price!= None:
+        inventory[item_id].price= item.price
+    if item.brand!= None:
+        inventory[item_id].brand= item.brand
+
     return inventory[item_id]
