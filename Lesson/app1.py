a9c8a68aaffcd0f60793daacc212cf125db3d8be
Ian Njari <iannjari@gmail.com>
2021-07-13 13:32:40 +0300

    Status codes and HTTPExceptions

--- Lesson/app1.py
+++ Lesson/app1.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 from fastapi.param_functions import Query
 from typing import Optional
 from pydantic import BaseModel
@@ -33,18 +33,19 @@ inventory= {
 def get_item(item_id:int = Path(None,description='The ID of the item you would like to view',gt=0)):
     if item_id in inventory:
         return inventory[item_id]
-    return {'Data Not Found'}
+    raise HTTPException(status_code=404,detail='Data Not Found') 
+
 @app.get("/get-by-name")
 def get_item_by_name(name:str =Query(None,title='Name',description='Item name')):
     for item_id in inventory:
         if inventory[item_id].name == name:
             return inventory[item_id]
-    return {'Data Not Found'}
+    raise HTTPException(status_code=404,detail='Data Not Found') 
 
 @app.post("/create-item/{item_id}")
 def create_item(item_id:int,item:Item):
     if item_id in inventory:
-        return {'Error':'Item ID already exists'}
+       raise HTTPException(status_code=404,detail='Item ID already exists')
     inventory[item_id]= item
     return inventory[item_id]
 
@@ -52,7 +53,7 @@ def create_item(item_id:int,item:Item):
 def update_item(item_id:int,item:UpdateItem):
     
     if item_id not in inventory:
-        return {'Error':'Item ID does not exist'}
+        raise HTTPException(status_code=404,detail='Item ID does not exist')
     
     if item.name!= None:
         inventory[item_id].name= item.name
@@ -67,6 +68,6 @@ def update_item(item_id:int,item:UpdateItem):
 def delete_item(item_id:int= Query(...,description='Item ID of item to delete',gt=0)):
     
     if item_id not in inventory:
-        return {'Error':'Item ID does not exist'}
+        raise HTTPException(status_code=404,detail='Item ID does not exist')
     del inventory[item_id]
     return {'Sucessfully Deleted'}
